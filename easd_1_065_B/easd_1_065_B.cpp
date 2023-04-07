b075f386ab5cd1242d9e67fedb07ef149b8e1677
bimbim1214 <bimoadityapangestu@gmail.com>
2023-04-07 10:49:55 +0700

    menambahkan jumlah maksimum data dengan nim

---
+++ easd_1_065_B/easd_1_065_B.cpp
@@ -0,0 +1,12 @@
+//karena algoritma sangat penting dalam menyeselesaikan suatu masalah dalam program, karena algoritma ini merupakan langkah awal dalam mengurutkan suatu cara atau fungsi agar berjalan secara teratur.
+//array dan link list
+//ukuran input, jenis algoritma, penggunaan struktur data
+//quick sort karena lebih sedikit menggunakan memori dan mempunyai kompleksitas dengan 0(n log n)
+//buble sort-insertion sort-selection sort termasuk ke quadratic, quick sort dan merge sort termasuk ke loglinear
+//
+
+#include <iostream>
+using namespace std;
+
+const int max = 65 + 20 - 2 * 15 + 10 + 20; // jumlah maksimum data
+
